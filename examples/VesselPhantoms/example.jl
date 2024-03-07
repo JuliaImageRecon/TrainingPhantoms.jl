0ee0262c7fadd6a054cf5ba34e8a7a22a997010b
Paul Jürß <p.juerss@uke.de>
2024-03-07 13:07:13 +0100

    Update vessel example to new interface

--- examples/VesselPhantoms/example.jl
+++ examples/VesselPhantoms/example.jl
@@ -16,7 +16,7 @@ GLMakie.activate!()
 rng = StableRNGs.StableRNG(123)
 N = (51,51,51)
 phantom = vesselPhantom(N; 
-    start=(1, N[2]/2, N[3]/2), angle_xy=0.0, angle_xz=0.0, 
+    start=(1, N[2]/2, N[3]/2), angles=(0.0,0.0), 
     diameter=2.5, split_prob=0.4, change_prob=0.3, 
     max_change=0.3, splitnr=1, max_number_splits=1, rng=rng)
 
